4f4ce8d48eabb094b6c862b0ca164e43a913180d
Nelson Durrant <snelsondurrant@gmail.com>
2026-01-13 11:36:56 -0700

    Fix for QEMU seg fault

--- coug_ws/src/coug_gui/launch/mapviz.launch.py
+++ coug_ws/src/coug_gui/launch/mapviz.launch.py
@@ -60,7 +60,7 @@ def generate_launch_description():
                 executable="initialize_origin.py",
                 name="initialize_origin",
                 remappings=[
-                    ("fix", "/origin"),  # set by 'navsat_preprocessor_node'
+                    ("fix", "/origin"),
                 ],
                 parameters=[{"use_sim_time": use_sim_time}],
             ),
