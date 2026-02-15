5a64070f8a7477f7bea5b76c7e0e6e98863002aa
Nelson Durrant <snelsondurrant@gmail.com>
2026-02-14 17:29:51 -0700

    Atomic Docker fix

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
-# Created by Nelson Durrant, Jan 2026
+# Copyright (c) 2026 BYU FROST Lab
 #
-# Runs automatic commands on Docker container startup
-# - This script won't throw any errors, but it will crash immediately if any command fails
-# - You can view the output of this script by running 'docker logs cougars-ct'
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 set -e
 
 if [ "$CI" == "true" ]; then
+    echo "CI environment detected. Skipping..."
     exec "$@"
 fi
 
@@ -34,20 +43,20 @@ find "/home/$DOCKER_USER" \
     -not -user "$DOCKER_USER" \
     -exec chown -R $DOCKER_USER:$DOCKER_USER {} + 2>/dev/null || true
 
+# Install external ROS packages (vcs)
+git config --system --add safe.directory "*"
+if curl -s --head --connect-timeout 5 https://github.com | grep "200" > /dev/null; then
+    echo "Updating vcs repositories..."
+    gosu $DOCKER_USER vcs import /home/$DOCKER_USER/ros2_ws/src < /home/$DOCKER_USER/ros2_ws/src/packages.repos
+    gosu $DOCKER_USER vcs custom /home/$DOCKER_USER/ros2_ws/src --git --args submodule update --init --recursive
+else
+    echo "No network connection. Skipping..."
+fi
+
 # Set up SSH (host keys)
 if [ -d "/home/$DOCKER_USER/.ssh" ]; then
     cat /home/$DOCKER_USER/.ssh/*.pub >> /home/$DOCKER_USER/.ssh_internal/authorized_keys 2>/dev/null || true
     sort -u /home/$DOCKER_USER/.ssh_internal/authorized_keys -o /home/$DOCKER_USER/.ssh_internal/authorized_keys
-fi
-
-# Install external ROS packages (vcs)
-git config --system --add safe.directory "*"
-if curl -s --head --connect-timeout 5 https://github.com | grep "200" > /dev/null; then
-    echo "Network found. Updating vcs repositories..."
-    gosu $DOCKER_USER vcs import /home/$DOCKER_USER/ros2_ws/src < /home/$DOCKER_USER/ros2_ws/src/cougars.repos
-    gosu $DOCKER_USER vcs custom /home/$DOCKER_USER/ros2_ws/src --git --args submodule update --init --recursive
-else
-    echo "No network connection. Skipping vcs repository updates."
 fi
 
 # Start the SSH server
--- holoocean/docker/entrypoint.sh
+++ holoocean/docker/entrypoint.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
-# Created by Nelson Durrant, Jan 2026
+# Copyright (c) 2026 BYU FROST Lab
 #
-# Runs automatic commands on Docker container startup
-# - This script won't throw any errors, but it will crash immediately if any command fails
-# - You can view the output of this script by running 'docker logs holoocean-ct'
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 set -e
 
