f7d68b854c8006b12748029bee552b6fe6a8bb55
Nelson Durrant <snelsondurrant@gmail.com>
2026-02-17 09:40:00 -0700

    Test new dev environment

--- .github/assets/easter_egg.py
+++ .github/assets/easter_egg.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python3
-# Created by Nelson Durrant (w Gemini 3 Pro), Feb 2025
+# Copyright (c) 2026 BYU FROST Lab
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 from asciimatics.effects import Print, Stars, Effect
 from asciimatics.renderers import ColourImageFile, FigletText, SpeechBubble
@@ -32,12 +44,8 @@ class Background(Effect):
 
 def display(screen):
     """
-    Displays an ASCII art display on startup.
-
-    :author: Nelson Durrant (w Gemini 3 Pro)
-    :date: Feb 2026
+    Display the easter egg.
     """
-
     scenes = []
     renderer = ColourImageFile(
         screen, "wave.gif", screen.height, uni=screen.unicode_aware
