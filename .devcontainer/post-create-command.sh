688101cd49562945f5ad39177bedb91892ad4c52
Nelson Durrant <snelsondurrant@gmail.com>
2026-02-17 17:11:06 -0700

    Better post-create-command

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -17,4 +17,4 @@ set -e
 
 vcs import packages < cougars.repos
 vcs custom --git --args submodule update --init --recursive
-find . -maxdepth 4 -name '.pre-commit-config.yaml' -execdir pre-commit install \;
+find . -maxdepth 4 -name '.pre-commit-config.yaml' -execdir pre-commit install --install-hooks \;
